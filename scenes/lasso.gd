e4c65b64b559e46f460ddf39b8865dd7ab8fc9e2
Owen "Q-Bit" Good <owen2013@gmail.com>
2025-07-31 16:26:31 -0400

    added crouch and slide

--- scenes/lasso.gd
+++ scenes/lasso.gd
@@ -2,18 +2,25 @@ extends CharacterBody2D
 
 @export var speed := 600.0
 @export var max_distance := 100.0
-#@export var return_duration := 30.0
 
 @export var gravity := 980.0
 
-@export var rancher = CharacterBody2D
+@export var lasso_pin = CharacterBody2D
+@export var connected_critter = CharacterBody2D
 
 var is_throwing := false
 var is_returning := false
+var is_connecting := false
+var is_lassoed := false
+
+func _ready() -> void:
+	visible = false
 
 func _physics_process(delta: float) -> void:
 	var is_throw_beginning := Input.is_action_just_pressed(&"lasso") and not is_throwing and not is_returning
+
 	if is_throw_beginning:
+		visible = true
 		velocity.x = speed
 		is_throwing = true
 
@@ -24,43 +31,38 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 	elif is_returning:
-		#testtest()
-
-		#position = position.lerp(rancher.position, 6.0 * delta)
-		#if position - rancher.position < Vector2(1.0, 1.0):
-			#position = rancher.position
-			#is_returning = false
-
-		#if is_equal_approx(position.x, rancher.position.x) and is_equal_approx(position.y, rancher.position.y):
-			#position = rancher.position
-			#is_returning = false
-		#else:
-			#position.lerp(rancher.position, 0.5)
-
-		if position - rancher.position < Vector2(1.0, 1.0):
-			position = rancher.position
+		if position - lasso_pin.global_position < Vector2(1.0, 1.0):
+			position = lasso_pin.global_position
 			velocity = Vector2.ZERO
 			is_returning = false
+			visible = false
 		else:
-			var return_velocity : Vector2 = (rancher.position - position)
+			var return_velocity : Vector2 = (lasso_pin.global_position - position)
 			velocity = (return_velocity.normalized() * 100.0 * speed * delta)
 			move_and_slide()
 
+	elif is_connecting:
+		if position - connected_critter.position < Vector2(1.0, 1.0):
+			position = connected_critter.position
+			velocity = Vector2.ZERO
+			is_lassoed = true
+			visible = false
+		else:
+			var connecting_velocity : Vector2 = (lasso_pin.global_position - position)
+			velocity = (connecting_velocity.normalized() * 100.0 * speed * delta)
+			move_and_slide()
+
+	elif is_lassoed:
+		position = connected_critter.position
+
 	else:
-		position = rancher.position
+		position = lasso_pin.global_position
 
-func testtest() -> void:
-	var icon = Sprite2D.new()
-	$LassoSprite.texture = icon
+func _on_critter_collision_body_entered(body: Node2D) -> void:
+	connected_critter = body
+	is_connecting = true
 
-#func return_to_rancher() -> void:
-		#var tween := create_tween()
-		#tween.set_trans(Tween.TRANS_CUBIC)
-		#tween.tween_property(self, ^":position", rancher.position, return_duration)
-		#await tween.finished
-		#is_returning = false
-
-func _on_area_2d_body_entered(_body: Node2D) -> void:
+func _on_wall_collision_body_entered(_body: Node2D) -> void:
 	if is_throwing:
 		is_throwing = false
 		is_returning = true
